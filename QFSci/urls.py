0d521db5e4a73e28cfb6007b1a573fb64b318379
WayneSkywalker <GrandCharge@outlook.com>
2020-05-26 02:08:21 +0700

    change admin url

--- QFSci/urls.py
+++ QFSci/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('', include('QFSci_manager.api.urls_info')),
     path('', include('QFSci_manager.api.urls_user')),
 ]
