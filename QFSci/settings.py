bc65e47b023f256a995eb989ad742280d79b1407
WayneSkywalker <GrandCharge@outlook.com>
2020-05-26 11:34:03 +0700

    django admin css problem #2

--- QFSci/settings.py
+++ QFSci/settings.py
@@ -136,10 +136,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
     'https://qfsci-6047a.web.app',
